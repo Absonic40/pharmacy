3a6e11af9c644b405338a1127337dee853dd9dd7
Absonic <mashiabolfazl@gmail.com>
2025-05-19 12:13:23 +0330

    Update index.tsx

--- pharmacy-app/src/index.tsx
+++ pharmacy-app/src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import './index.css'; // اطمینان از وارد کردن فایل CSS
+import './index.css'; // وارد کردن فایل CSS برای استایل‌دهی
 import App from './App';
 
+// ایجاد یک نمونه از QueryClient برای مدیریت درخواست‌های سرور
 const queryClient = new QueryClient();
+
+// گرفتن ریشه DOM برای رندر کردن اپلیکیشن
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
+    {/* فراهم کردن QueryClientProvider برای مدیریت درخواست‌های سرور */}
     <QueryClientProvider client={queryClient}>
+      {/* کامپوننت اصلی اپلیکیشن */}
       <App />
     </QueryClientProvider>
   </React.StrictMode>
